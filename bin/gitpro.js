6add46bd75d55b808a5ad652a70414d257236abc
Alex Doutsinis <Alexiosdut@gmail.com>
2022-05-02 18:51:16 +0300

    create 'switch' command

--- bin/gitpro.js
+++ bin/gitpro.js
@@ -5,6 +5,7 @@ import { conflicts } from '../commands/conflicts.js'
 import { discard } from '../commands/discard.js'
 import { gitUser } from '../commands/gitUser.js'
 import { refresh } from '../commands/refresh.js'
+import { switchBranch } from '../commands/switchBranch.js'
 
 program.version('1.0.0')
 
@@ -14,5 +15,6 @@ conflicts(program)
 refresh(program)
 gitUser(program)
 discard(program)
+switchBranch(program)
 
 program.parse(process.argv)
--- commands/switchBranch.js
+++ commands/switchBranch.js
@@ -1,5 +1,8 @@
 import shell from 'shelljs'
+import { localBranchExist } from '../utils/localBranchExist.js'
+import { logMessage } from '../utils/logMessage.js'
 import { requireGit } from '../utils/requireGit.js'
+import { stashDoThenPop } from '../utils/stashDoThenPop.js'
 
 export function switchBranch(program) {
   program
@@ -9,7 +12,20 @@ export function switchBranch(program) {
         'Switch branches easily. Takes care of stashing changes and creating a new branch if required.',
     })
     .argument({ name: 'branch', isRequired: true })
-    .action(branch => {
+    .action(async branch => {
       requireGit(shell)
+
+      await stashDoThenPop(shell, () => {
+        if (localBranchExist(shell, branch)) {
+          shell.echo(logMessage.info + `Branch '${branch}' already exists. Switch to it`)
+          shell.exec(`git checkout ${branch}`, { silent: true })
+        } else {
+          shell.echo(
+            logMessage.info +
+              `Branch '${branch}' does not exists. Create the branch and switch`,
+          )
+          shell.exec(`git checkout -b ${branch}`, { silent: true })
+        }
+      })
     })
 }
