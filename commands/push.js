f6fe481dfe72519c3cd921e1dd6438f76fc217e8
Alex Doutsinis <Alexiosdut@gmail.com>
2022-06-20 22:28:08 +0300

    minor change

--- commands/push.js
+++ commands/push.js
@@ -67,7 +67,6 @@ export function push(program) {
             return
           }
 
-          shell.echo(logMessage.info + `Pushing changes to '${currentLocalBranch}'`)
           shell.exec(`git push origin -u ${currentLocalBranch}`, {
             silent: true,
           })
