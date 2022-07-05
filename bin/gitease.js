321a30d6088f106745d4490457dbd8584898e34f
Alex Doutsinis <Alexiosdut@gmail.com>
2022-07-05 22:26:17 +0300

    update version

--- bin/gitease.js
+++ bin/gitease.js
@@ -11,7 +11,7 @@ import { refresh } from '../commands/refresh.js'
 import { showGitUser } from '../commands/showGitUser.js'
 import { switchBranch } from '../commands/switchBranch.js'
 
-program.version('1.0.4')
+program.version('1.0.5')
 
 // commands
 commit(program)
