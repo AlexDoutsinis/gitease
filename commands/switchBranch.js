a0e14fe6cda99d1326339045d18691ae1aadcb33
Alex Doutsinis <Alexiosdut@gmail.com>
2022-07-20 01:07:15 +0300

    switch command: stash changes only if -mv option is provided

--- commands/switchBranch.js
+++ commands/switchBranch.js
@@ -11,21 +11,46 @@ export function switchBranch(program) {
       description:
         'Switch branches easily. Takes care of stashing changes and creating a new branch if required.',
     })
-    .argument({ name: 'branch', isRequired: true })
-    .action(async branch => {
+    .options([
+      {
+        name: { full: 'branch', short: 'b' },
+        description: 'The branch to switch',
+        isRequired: true,
+        valueIsRequired: true,
+        acceptMultipleValues: false,
+      },
+      {
+        name: { full: 'move', short: 'mv' },
+        description: 'Move changes from current to target branch',
+        isRequired: false,
+        valueIsRequired: false,
+        acceptMultipleValues: false,
+      },
+    ])
+    .action(async ({ branch, move }) => {
       requireGit(shell)
 
-      await stashDoThenPop(shell, () => {
+      const switchBranch = () => {
         if (localBranchExist(shell, branch)) {
           shell.echo(logMessage.info + `Branch '${branch}' already exists. Switch to it`)
-          shell.exec(`git checkout ${branch}`, { silent: true })
+          shell.exec(`git checkout ${branch}`)
         } else {
           shell.echo(
             logMessage.info +
               `Branch '${branch}' does not exists. Create the branch and switch`,
           )
-          shell.exec(`git checkout -b ${branch}`, { silent: true })
+          shell.exec(`git checkout -b ${branch}`)
         }
+      }
+
+      if (!move) {
+        switchBranch()
+
+        return
+      }
+
+      await stashDoThenPop(shell, () => {
+        switchBranch()
       })
     })
 }
