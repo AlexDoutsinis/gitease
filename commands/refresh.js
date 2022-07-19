80e3e4f0fa03f4bff13f6697b2d9b7882a3e123e
Alex Doutsinis <Alexiosdut@gmail.com>
2022-07-19 22:28:10 +0300

    refresh command: major fix

--- commands/refresh.js
+++ commands/refresh.js
@@ -7,50 +7,63 @@ import { pullRemoteChangesIfNeeded } from '../utils/pullRemoteChangesIfNeeded.js
 import { localBranchExist } from '../utils/localBranchExist.js'
 import { logMessage } from '../utils/logMessage.js'
 import { stashDoThenPop } from '../utils/stashDoThenPop.js'
+import { branchExistOnRemote } from '../utils/branchExistOnRemote.js'
 
 export function refresh(program) {
   program
     .command({
       name: 'refresh',
-      description: 'Refreshes the current branch from a remote branch',
+      description: 'Refreshes the current local branch from a remote branch',
     })
     .argument({
       name: 'remoteBranch',
       description: 'The remote branch to pull from',
       isRequired: false,
     })
-    .action(async branch => {
+    .action(async remoteBranch => {
       requireGit(shell)
       const currentBranch = getCurrentLocalBranch(shell)
 
-      if (currentBranch == branch || !branch) {
+      if (currentBranch == remoteBranch || !remoteBranch) {
         await stashDoThenPop(shell, async () => {
           await pullRemoteChangesIfNeeded(shell, currentBranch)
         })
       } else {
-        if (!localBranchExist(shell, branch)) {
-          shell.echo(logMessage.error + `'${branch}' does not exist locally`)
+        if (!localBranchExist(shell, remoteBranch)) {
+          shell.echo(logMessage.error + `'${remoteBranch}' does not exist locally`)
+          return
+        }
+
+        if (!branchExistOnRemote(shell, remoteBranch)) {
+          shell.echo(logMessage.error + `'${remoteBranch}' does not exist on remote`)
+          return
+        }
+
+        if (branchExistOnRemote(shell, currentBranch)) {
+          shell.echo(
+            logMessage.error +
+              `You are currently on '${currentBranch}' branch which exists on remote. Please make sure that you are on a local branch in order to continue with the command`,
+          )
           return
         }
 
         await stashDoThenPop(shell, async () => {
-          const currentBranchHasChanges = await pullRemoteChangesIfNeeded(
+          checkoutBranch(shell, remoteBranch)
+          const remoteBranchHasChanges = await pullRemoteChangesIfNeeded(
             shell,
-            currentBranch,
+            remoteBranch,
           )
-          checkoutBranch(shell, branch)
-          const branchHasChanges = await pullRemoteChangesIfNeeded(shell, branch)
           checkoutBranch(shell, currentBranch)
 
-          if (!currentBranchHasChanges && !branchHasChanges) {
+          if (!remoteBranchHasChanges) {
             shell.echo(
               logMessage.warning +
-                `There are no changes to pull from '${currentBranch}' and '${branch}' branches so the process ends here`,
+                `There are no changes to pull from '${remoteBranch}' branch. '${currentBranch}' is fresh!`,
             )
             return
           }
 
-          await rebaseBranch(shell, branch)
+          await rebaseBranch(shell, remoteBranch)
         })
       }
     })
