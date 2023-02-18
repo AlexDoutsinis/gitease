fd7dd600458e6581634b84ee2ead89822594cd33
Alex Doutsinis <Alexiosdut@gmail.com>
2023-02-18 14:59:08 +0200

    delete pullOption reusable object as it has just one reference

--- options/pullOption.js
+++
@@ -1,7 +0,0 @@
-export const pullOption = {
-  name: { full: 'pull', short: 'p' },
-  description: 'pull remote changes before creating the commit',
-  acceptMultipleValues: false,
-  isRequired: false,
-  valueIsRequired: false,
-}
